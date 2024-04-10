fd36e84c07eec99c8b568737d21d4db035bc1b8b
thom364e <thomas.jensen@hotmail.dk>
2024-04-10 18:14:49 +0200

    Updated plots and data cleanin

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -14,3 +14,21 @@ def keep_regs(df, regs):
         df = df.loc[I == False] # keep everything else
     
     return df
+
+class gpr_clean():
+
+    def __init__(self, df):
+        self.data = df
+        self.gpr = self.data.copy()
+        self.gpr_long = self.drop_cols()
+
+    def drop_cols(self):
+        # Selecting the columns with the country specific historical GPR index
+        gpr = self.gpr
+        gpr = gpr[[col for col in gpr.columns if col.startswith('GPRHC_')]]
+
+        # Melt the DataFrame to long format
+        gpr_long = gpr.reset_index().melt(id_vars='Date', var_name='Country', value_name='GPRH')
+        gpr_long.set_index('Date', inplace=True)
+        
+        return gpr_long
