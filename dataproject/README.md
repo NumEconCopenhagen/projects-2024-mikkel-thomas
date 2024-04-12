88a17a60e4d5dfba37b2d77f86c0901f35233834
thom364e <thomas.jensen@hotmail.dk>
2024-04-12 20:47:45 +0200

    Updated structure and cleaned up code and charts

--- dataproject/README.md
+++ dataproject/README.md
@@ -1,14 +1,30 @@
 # Data analysis project
 
-Our project is titled **PROJECT TITLE** and is about EXPLAIN.
+Our project is titled **Have the World Become Less Peaceful? An Emirical Analysis of Geopolitical Trends** and is about the development of war and peach over time in the world.
 
-The **results** of the project can be seen from running [dataproject.ipynb](dataproject.ipynb).
+The results of the project can be seen from running [dataproject.ipynb](dataproject.ipynb). This file will generate five different charts each telling a piece of the story about war and peace in the world.
+
+The respositiory also contains the dataproject.py file, which contains classes and functions to clean and merge data. The folder `misc` contain old stuff and should not be used.
 
 We apply the **following datasets**:
 
-1. dataX.csv (*source*) 
-1. dataY.csv (*source*)
+1. GPR_quarter.xlsx ([Caldara and Iacoviello (2022)](https://www.matteoiacoviello.com/gpr.htm))
+1. IdealpointestimatesAll_Sep2023.csv ([Bailey, Strezhnev and Voeten (2017)](https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/LEJUQZ))
+1. military_gdp_spend.csv ([World Bank WDI](https://data.worldbank.org/indicator/MS.MIL.XPND.GD.ZS))
+1. population.csv ([World Bank WDI](https://data.worldbank.org/indicator/SP.POP.TOTL))
+1. war_deaths.csv ([Our World in Data](https://ourworldindata.org/explorers/conflict-data?facet=none&Conflict+type=All+armed+conflicts&Measure=Conflict+deaths&Conflict+sub-type=By+sub-type&Data+source=Uppsala+Conflict+Data+Program&Sub-measure=Regional+data&country=~OWID_WRL))
+
+In addition to the above, we also use two lookup tables to match varible names to countries names (country_lookup.xlsx).
+
+The project is structured as follows:
+1. `dataproject.ipynb` contains all our charts
+1. `dataproject.py` does the heavy lifting in the form of data cleaning and plotting 
+
 
 **Dependencies:** Apart from a standard Anaconda Python 3 installation, the project requires the following installations:
 
 ``pip install matplotlib-venn``
+
+``pip install joypy``
+
+``pip install pycountry_convert``
--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -7,6 +7,7 @@ import pycountry_convert as pc
 from pandas.api.types import CategoricalDtype
 from joypy import joyplot
 from matplotlib import cm
+import altair as alt
 
 def keep_regs(df, regs):
     """ Example function. Keep only the subset regs of regions in data.
@@ -26,8 +27,19 @@ def keep_regs(df, regs):
     return df
 
 class gpr_clean():
+    """
+    Class to clean the Geopolitical Risk Index (GPR) data
+    """
 
     def __init__(self, df):
+        """
+        Initialize the class with the DataFrame to be cleaned
+
+        Args:
+            df (pd.DataFrame): DataFrame to be cleaned
+        Returns:
+            None
+        """
         self.data = df
         self.gpr = self.data.copy()
         self.gpr_long = self.drop_cols()
@@ -35,6 +47,13 @@ class gpr_clean():
         self.gpr_long = self.stadardize_gpr_apply()
 
     def drop_cols(self):
+        """
+        Drop columns that are not needed for the analysis
+
+        Returns:
+            gpr_long (pd.DataFrame): DataFrame with the relevant columns
+        """
+
         # Selecting the columns with the country specific historical GPR index
         gpr = self.gpr
         gpr = gpr[[col for col in gpr.columns if col.startswith('GPRHC_')]]
@@ -46,6 +65,12 @@ class gpr_clean():
         return gpr_long
 
     def country_lookup(self):
+        """ 
+        Merge the DataFrame with the country lookup table that maps the original variable names to the full country names
+
+        Returns:
+            gpr_long (pd.DataFrame): DataFrame with the full country names and the GPR index
+        """
         filename = 'country_lookup.xlsx'
         country_lookup = pd.read_excel(filename)
 
@@ -56,26 +81,60 @@ class gpr_clean():
         return gpr_long
     
     def standardize_gpr(self,df, country_col, gpr_col):
+        """
+        Standardize the GPR index by country. This function is used in the 'stadardize_gpr_apply' method
+        Args:
+            df (pd.DataFrame): DataFrame to be cleaned
+            country_col (str): Name of the column with the country names
+            gpr_col (str): Name of the column with the GPR index
+        Returns:
+            df (pd.DataFrame): DataFrame with the standardized GPR index as a column
+        """
+
         df[gpr_col] = df.groupby(country_col)[gpr_col].transform(lambda x: (x - x.mean()) / x.std())
         return df
 
     def stadardize_gpr_apply(self):
+        """
+        Standardize the GPR index by country
+        Args:
+            None
+        Returns:
+            gpr_long (pd.DataFrame): DataFrame with the standardized GPR index
+        """
         gpr_long = self.standardize_gpr(self.gpr_long, 'Country_name', 'GPRH')
         
         gpr_long['Date'] = gpr_long['Date'].dt.date
         return gpr_long
 
     def __call__(self):
+        """"
+        Return the cleaned DataFrame when the instance is called like a function
+        """
         # Return the cleaned DataFrame when the instance is called like a function
         return self.gpr_long
     
 class idealpoint_clean():
 
     def __init__(self, df):
+        """
+        Initialize the class with the DataFrame to be cleaned
+        Args:
+            df (pd.DataFrame): DataFrame to be cleaned
+        Returns:
+            None
+        """
         self.df = df
         self.long_df_q = self.clean_idealpoint()
 
     def clean_idealpoint(self):
+        """
+        Function to clean the ideal point data
+        Args:
+            None
+        Returns:
+            long_df_q (pd.DataFrame): DataFrame with the ideal point data in long format and resampled to quarterly frequency
+        """
         pivot_df = self.df.pivot_table(index='session', columns='Countryname', values='IdealPointAll')
         pivot_df.columns.name = None
     
@@ -104,144 +163,6 @@ class idealpoint_clean():
         # Return the cleaned DataFrame when the instance is called like a function
         return self.long_df_q
     
-class dots_clean():
-
-    def __init__(self, df):
-        self.df = df
-        self.dots_imf = self.df.copy()
-        self.dots_imf = self.fill_nan()
-        self.dots_imf = self.new_vars()
-        self.dots_imf = self.new_names()
-        self.dots_imf = self.slim_data()
-
-    def fill_nan(self): 
-        dots_imf = self.dots_imf.fillna(0)
-
-        return dots_imf
-    
-    def new_vars(self):
-        dots_imf = self.dots_imf
-        dots_imf['Trade'] = self.dots_imf['Export'] + self.dots_imf['Import']
-
-        dots_imf['tot_trade'] = dots_imf.groupby(['Country_Name', 'Date'])['Trade'].transform('sum')
-        dots_imf['trade_share'] = dots_imf['Trade'] / dots_imf['tot_trade']
-        dots_imf['check'] = dots_imf.groupby(['Country_Name', 'Date'])['trade_share'].transform('sum')
-        dots_imf['Date'] = pd.to_datetime(dots_imf['Date']).dt.to_period('Q').dt.start_time
-        dots_imf = dots_imf.rename(columns={'Country_Name': 'imf_name'})
-        dots_imf = dots_imf.rename(columns={'Counterpart_Country_Name': 'imf_name_counterpart'})
-
-        return dots_imf
-    
-    def new_names(self):
-
-        dots_imf = self.dots_imf
-        # Merging the DataFrame with the lookup tables
-        filename = 'imf_name_lookup.xlsx'
-        imf_lookup = pd.read_excel(filename)
-        filename = 'imf_name_cp_lookup.xlsx'
-        imf_lookup_cp = pd.read_excel(filename)
-        dots_imf = dots_imf.reset_index().merge(imf_lookup, on='imf_name', how='left')
-        dots_imf = dots_imf.reset_index().merge(imf_lookup_cp, on='imf_name_counterpart', how='left')
-
-        # Rename the columns
-        dots_imf = dots_imf.rename(columns={'Country_name_x': 'country'})
-        dots_imf = dots_imf.rename(columns={'Country_name_y': 'counterpart'})
-        # dots_imf.head(5)
-        return dots_imf
-    
-    def slim_data(self):
-        # Select the relevant columns
-        dots_imf = self.dots_imf[['Date','country', 'counterpart', 'trade_share']]
-        dots_imf = dots_imf.rename(columns={'country': 'Country_name', 'counterpart': 'Counterpart_name'})
-
-        dots_imf.set_index(['Date', 'Country_name'], inplace=True)
-        dots_imf = dots_imf.sort_values(['Country_name', 'Date'])
-
-        return dots_imf
-    
-    def __call__(self):
-        # Return the cleaned DataFrame when the instance is called like a function
-        return self.dots_imf
-    
-class merge_dots_gpr():
-
-    def __init__(self, df_dots, df_gpr):
-        self.df_dots = df_dots
-        self.df_gpr = df_gpr
-
-        self.gpr_trade = self.run()
-
-    def merge_data(self):
-        df_dots = self.df_dots
-        df_gpr = self.df_gpr
-        merged_df = df_dots.reset_index().merge(df_gpr, on=['Country_name', 'Date'], how='left')
-        merged_df = merged_df.rename(columns={'Country_name': 'name', 'Counterpart_name': 'Country_name'})
-
-        merged_df_2 = merged_df.reset_index().merge(df_gpr, on=['Country_name', 'Date'], how='left')
-
-        return merged_df_2
-    
-    def trade_weighted_gpr(self):
-
-        merged_df_2 = self.merged_df_2
-        merged_df_2['product'] = merged_df_2['trade_share'] * merged_df_2['GPRH_y']
-        merged_df_2['sumproduct'] = merged_df_2.groupby(['Date', 'name'])['product'].transform('sum')
-        merged_df_2['GPRH_trade_weight'] = merged_df_2['sumproduct'] + merged_df_2['GPRH_x']
-
-        merged_df_2_no_dub = merged_df_2.drop_duplicates(subset=['Date', 'name', 'GPRH_trade_weight'])
-
-        return merged_df_2_no_dub
-
-    def slim_data(self):
-        merged_df_2_no_dub = self.merged_df_2_no_dub
-        gpr_trade = merged_df_2_no_dub[['Date','name', 'GPRH_trade_weight']]
-        gpr_trade.set_index(['Date', 'name'], inplace=True)
-        gpr_trade.reset_index(inplace=True)
-
-        gpr_trade = gpr_trade.rename(columns={'name': 'Country_name'})
-
-        return gpr_trade
-    
-    def run(self):
-        self.merged_df_2 = self.merge_data()
-        self.merged_df_2_no_dub = self.trade_weighted_gpr()
-        self.gpr_trade = self.slim_data()
-
-        return self.gpr_trade
-    
-    def __call__(self):
-        # Return the cleaned DataFrame when the instance is called like a function
-        return self.gpr_trade
-
-class merge_data():
-
-    def __init__(self, df_gpr, df_idealpoint):
-        self.df_gpr = df_gpr
-        self.df_idealpoint = df_idealpoint
-
-        self.merged_df = self.merge_data()
-
-    def merge_data(self):
-        merged_df = self.df_gpr.reset_index().merge(self.df_idealpoint, on=['Country_name', 'Date'], how='left')
-        merged_df = merged_df[merged_df['Date'].dt.year >= 1946]
-        merged_df.drop(columns='index', inplace=True)
-        
-        merged_df.dropna(inplace=True)
-        merged_df.set_index(['Date', 'Country_name'], inplace=True)
-
-        # Calculate the average of 'IdealPointAll' by 'Country_name'
-        average_ideal_point = merged_df.groupby(level='Country_name')['IdealPointAll'].mean()
-
-        # Create a new column that assigns 1 if the average of 'IdealPointAll' is positive and 0 otherwise
-        merged_df['NewColumn'] = merged_df.index.get_level_values('Country_name').map(lambda x: 1 if average_ideal_point[x] > 0 else 0)
-        
-        return merged_df
-    
-    def __call__(self):
-        # Return the cleaned DataFrame when the instance is called like a function
-        return self.merged_df
-    
-
 def heatmap_gpr(df_gpr):
     df_heatmap = df_gpr.pivot_table(index='Country_name', columns='Date', values='GPRH')
 
@@ -259,7 +180,7 @@ def heatmap_gpr(df_gpr):
     heatmap = sns.heatmap(df_heatmap, cmap='coolwarm', vmin=vmin, vmax=vmax)
 
     cbar = heatmap.collections[0].colorbar
-    cbar.set_label('Geopolitical Risk Index (GPRH)', fontsize=16)
+    cbar.set_label('Geopolitical Risk Index (GPRH)', fontsize=13)
 
     # New code to modify x-tick labels
     ax = plt.gca()
@@ -275,10 +196,10 @@ def heatmap_gpr(df_gpr):
 
     plt.xticks(new_locs, new_labels)
 
-    ax.set_ylabel('Country', fontsize=16)
-    ax.set_xlabel('Date', fontsize=16)
-    ax.set_title('Geopolitical risk has increased in recent years...', fontsize=16)
-
+    ax.set_ylabel('Country', fontsize=13)
+    ax.set_xlabel('Date', fontsize=13)
+    title = ax.set_title('Country Level Geopolitical Risk Index (1990-2023)', fontsize=13)
+    # title.set_position([-.18, 1])
     plt.show()
 
 def lineplot_idealpoints(df_idealpoint):
@@ -302,9 +223,9 @@ def lineplot_idealpoints(df_idealpoint):
     plt.xlim([datetime.date(1946, 12, 31), datetime.date(2021, 12, 31)])
 
     plt.grid(True, linestyle = '--', alpha = 0.5)
-    plt.xlabel('Year', fontsize = 16)
-    plt.ylabel('Ideal Point', fontsize = 16)
-    plt.title('The post Cold War convergence in geopoltical alignment has stalled...', fontsize = 16)
+    plt.xlabel('Year', fontsize = 13)
+    plt.ylabel('Ideal Point', fontsize = 13)
+    plt.title('Ideal Point Estimates Based on UN Voting Behaviour', fontsize = 13)
     plt.legend()
     plt.show()
 
@@ -416,6 +337,195 @@ def ridge_gpr(df_gpr):
         colormap=cm.coolwarm,
         figsize=(12, 8)
     )
-    plt.title('The 2020s could be a new above average decate of risk...', fontsize=16)
-    plt.xlabel('Average Geopolitical Risk Index, all countries', fontsize=16)
+    plt.title('Distribution of Global GPR index for Five-Year Periods (1900-2023)', fontsize=13)
+    plt.xlabel('Average Geopolitical Risk Index, all countries', fontsize=13)
     plt.show()
+
+def lineplot_war_deaths(df_war_deaths):
+    df_war_deaths = df_war_deaths[df_war_deaths['Entity'] == 'World']
+
+    df_war_deaths = df_war_deaths.drop(columns=['Code', 'Entity'])
+    df_war_deaths = df_war_deaths.melt(id_vars='Year', var_name='Type', value_name='Count')
+    # Converting the year column to datetime
+    df_war_deaths['Year'] = pd.to_datetime(df_war_deaths['Year'], format='%Y')
+
+    plot = alt.Chart(df_war_deaths).mark_circle(
+    opacity=0.8,
+    stroke='black',
+    strokeWidth=1,
+    strokeOpacity=0.4
+    ).encode(
+    alt.X('Year:T')
+        .title(None)
+        .scale(domain=['1989','2022']),
+    alt.Y('Type:N')
+        .title(None)
+        .sort(field="Count", op="sum", order='descending'),
+    alt.Size('Count:Q')
+        .scale(range=[0, 2500])
+        .title('Deaths')
+        .legend(clipHeight=30, format='s'),
+    alt.Color('Type:N').legend(None),
+    tooltip=[
+        "Type:N",
+        alt.Tooltip("Year:T", format='%Y'),
+        alt.Tooltip("Count:Q", format='~s')
+    ],
+    ).properties(
+    width=1.3*450,
+    height=320,
+    title=alt.Title(
+        text="Global Deaths from State-Based Wars (1989-2022)",
+        subtitle="The size of the bubble represents the total death count per year, by type of war",
+        anchor='start'
+    )
+    ).configure_axisY(
+    domain=False,
+    ticks=False,
+    offset=10
+    ).configure_axisX(
+    grid=False,
+    ).configure_view(
+    stroke=None
+    )
+
+    return plot
+
+# OLD CODE BELOW - NOT USED IN THE FINAL PROJECT
+
+class dots_clean():
+
+    def __init__(self, df):
+        self.df = df
+        self.dots_imf = self.df.copy()
+        self.dots_imf = self.fill_nan()
+        self.dots_imf = self.new_vars()
+        self.dots_imf = self.new_names()
+        self.dots_imf = self.slim_data()
+
+    def fill_nan(self): 
+        dots_imf = self.dots_imf.fillna(0)
+
+        return dots_imf
+    
+    def new_vars(self):
+        dots_imf = self.dots_imf
+        dots_imf['Trade'] = self.dots_imf['Export'] + self.dots_imf['Import']
+
+        dots_imf['tot_trade'] = dots_imf.groupby(['Country_Name', 'Date'])['Trade'].transform('sum')
+        dots_imf['trade_share'] = dots_imf['Trade'] / dots_imf['tot_trade']
+        dots_imf['check'] = dots_imf.groupby(['Country_Name', 'Date'])['trade_share'].transform('sum')
+        dots_imf['Date'] = pd.to_datetime(dots_imf['Date']).dt.to_period('Q').dt.start_time
+        dots_imf = dots_imf.rename(columns={'Country_Name': 'imf_name'})
+        dots_imf = dots_imf.rename(columns={'Counterpart_Country_Name': 'imf_name_counterpart'})
+
+        return dots_imf
+    
+    def new_names(self):
+
+        dots_imf = self.dots_imf
+        # Merging the DataFrame with the lookup tables
+        filename = 'imf_name_lookup.xlsx'
+        imf_lookup = pd.read_excel(filename)
+        filename = 'imf_name_cp_lookup.xlsx'
+        imf_lookup_cp = pd.read_excel(filename)
+        dots_imf = dots_imf.reset_index().merge(imf_lookup, on='imf_name', how='left')
+        dots_imf = dots_imf.reset_index().merge(imf_lookup_cp, on='imf_name_counterpart', how='left')
+
+        # Rename the columns
+        dots_imf = dots_imf.rename(columns={'Country_name_x': 'country'})
+        dots_imf = dots_imf.rename(columns={'Country_name_y': 'counterpart'})
+        # dots_imf.head(5)
+        return dots_imf
+    
+    def slim_data(self):
+        # Select the relevant columns
+        dots_imf = self.dots_imf[['Date','country', 'counterpart', 'trade_share']]
+        dots_imf = dots_imf.rename(columns={'country': 'Country_name', 'counterpart': 'Counterpart_name'})
+
+        dots_imf.set_index(['Date', 'Country_name'], inplace=True)
+        dots_imf = dots_imf.sort_values(['Country_name', 'Date'])
+
+        return dots_imf
+    
+    def __call__(self):
+        # Return the cleaned DataFrame when the instance is called like a function
+        return self.dots_imf
+    
+class merge_dots_gpr():
+
+    def __init__(self, df_dots, df_gpr):
+        self.df_dots = df_dots
+        self.df_gpr = df_gpr
+
+        self.gpr_trade = self.run()
+
+    def merge_data(self):
+        df_dots = self.df_dots
+        df_gpr = self.df_gpr
+        merged_df = df_dots.reset_index().merge(df_gpr, on=['Country_name', 'Date'], how='left')
+        merged_df = merged_df.rename(columns={'Country_name': 'name', 'Counterpart_name': 'Country_name'})
+
+        merged_df_2 = merged_df.reset_index().merge(df_gpr, on=['Country_name', 'Date'], how='left')
+
+        return merged_df_2
+    
+    def trade_weighted_gpr(self):
+
+        merged_df_2 = self.merged_df_2
+        merged_df_2['product'] = merged_df_2['trade_share'] * merged_df_2['GPRH_y']
+        merged_df_2['sumproduct'] = merged_df_2.groupby(['Date', 'name'])['product'].transform('sum')
+        merged_df_2['GPRH_trade_weight'] = merged_df_2['sumproduct'] + merged_df_2['GPRH_x']
+
+        merged_df_2_no_dub = merged_df_2.drop_duplicates(subset=['Date', 'name', 'GPRH_trade_weight'])
+
+        return merged_df_2_no_dub
+
+    def slim_data(self):
+        merged_df_2_no_dub = self.merged_df_2_no_dub
+        gpr_trade = merged_df_2_no_dub[['Date','name', 'GPRH_trade_weight']]
+        gpr_trade.set_index(['Date', 'name'], inplace=True)
+        gpr_trade.reset_index(inplace=True)
+
+        gpr_trade = gpr_trade.rename(columns={'name': 'Country_name'})
+
+        return gpr_trade
+    
+    def run(self):
+        self.merged_df_2 = self.merge_data()
+        self.merged_df_2_no_dub = self.trade_weighted_gpr()
+        self.gpr_trade = self.slim_data()
+
+        return self.gpr_trade
+    
+    def __call__(self):
+        # Return the cleaned DataFrame when the instance is called like a function
+        return self.gpr_trade
+    
+class merge_data():
+
+    def __init__(self, df_gpr, df_idealpoint):
+        self.df_gpr = df_gpr
+        self.df_idealpoint = df_idealpoint
+
+        self.merged_df = self.merge_data()
+
+    def merge_data(self):
+        merged_df = self.df_gpr.reset_index().merge(self.df_idealpoint, on=['Country_name', 'Date'], how='left')
+        merged_df = merged_df[merged_df['Date'].dt.year >= 1946]
+        merged_df.drop(columns='index', inplace=True)
+        
+        merged_df.dropna(inplace=True)
+        merged_df.set_index(['Date', 'Country_name'], inplace=True)
+
+        # Calculate the average of 'IdealPointAll' by 'Country_name'
+        average_ideal_point = merged_df.groupby(level='Country_name')['IdealPointAll'].mean()
+
+        # Create a new column that assigns 1 if the average of 'IdealPointAll' is positive and 0 otherwise
+        merged_df['NewColumn'] = merged_df.index.get_level_values('Country_name').map(lambda x: 1 if average_ideal_point[x] > 0 else 0)
+        
+        return merged_df
+    
+    def __call__(self):
+        # Return the cleaned DataFrame when the instance is called like a function
+        return self.merged_df
