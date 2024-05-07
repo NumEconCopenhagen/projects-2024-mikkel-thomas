dd27cd8c2f3e16173cfc67faa2ab50c7e2ec1896
Mikkellyngholm <tzn327@alumni.ku.dk>
2024-05-07 15:08:27 +0200

    Looked at code in module

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,4 +1,90 @@
+from types import SimpleNamespace
+import numpy as np
 from scipy import optimize
+
+class RamseyModelClass1():
+    ''' Ramsey model class '''
+
+    def __init__(self,do_print=True):
+        """ create the model """
+
+        if do_print: print('initializing the model:')
+
+        self.par = SimpleNamespace()
+        self.ss = SimpleNamespace()
+        self.path = SimpleNamespace()
+
+        if do_print: print('calling .setup()')
+        self.setup()
+
+        if do_print: print('calling .allocate()')
+        self.allocate()
+
+    def setup(self):
+        """ baseline parameters """
+
+        par = self.par
+
+        # a. household
+        par.sigma = 2.0
+        par.beta = np.nan
+
+        # b. firms
+        par.Gamma = np.nan
+        par.production_function = 'cobb-douglas'
+        par.alpha = 0.30
+        par.theta = 0.05
+        par.delta = 0.05
+
+        # c. initial
+        par.K_lag_ini = 1.0
+
+        # d. misc
+        par.solver = 'scpiy'
+        par.Tpath = 500
+
+    def allocate(self):
+        """ allocate arrays for transition path """
+
+        par = self.par
+        path = self.path
+
+        allvarnames = ['Gamma','K','C','rk','w','r','Y','K_lag']
+        for varname in allvarnames:
+            path.__dict__[varname] =  np.nan*np.ones(par.Tpath)
+
+    def find_steady_state(self,KY_ss,do_print=True):
+        """ find steady state """
+
+        par = self.par
+        ss = self.ss
+
+        # a. unpack
+        K_ss,Y_ss = KY_ss
+
+        # b. solve for C_ss
+        ss.C = Y_ss - par.delta*K_ss
+
+        # c. solve for Gamma
+        ss.Gamma = par.alpha*Y_ss/K_ss
+
+        # d. solve for r_ss
+        ss.r = ss.Gamma - par.delta
+
+        # e. solve for w_ss
+        ss.w = (1-par.alpha)*Y_ss
+
+        # f. solve for rk_ss
+        ss.rk = ss.r + par.delta
+
+        # g. store
+        ss.K = K_ss
+        ss.Y = Y_ss
+
+        if do_print: print(ss.__dict__)
+
+        return ss
+
 
 def solve_ss(alpha, c):
     """ Example function. Solve for steady state k. 
